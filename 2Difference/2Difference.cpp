e8d159afe9091545d29694d52d5cd53bd0ec5c9a
Piyush_Dubey <pd3429978@gmail.com>
2026-01-07 17:16:23 +0530

    Add files via upload

---
+++ 2Difference/2Difference.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool findthedifference(vector<int>&arr,int target){
+    int start=0;
+    int end=1;
+     int n = arr.size();
+    while(end<n){
+     int sum=arr[end]-arr[start];
+     if(sum==target){
+        return true;
+     }
+     else if (sum<target){
+        end++;
+     }
+     else{
+        start++;
+     }
+      if (start == end) {
+            end++;
+    }
+}
+    return false;
+
+}
+int main ()
+{
+ vector<int> arr={2,3,5,10,50,80};
+ int target =41;
+ if(findthedifference(arr,target)){
+    cout<<"Pair with the difference is found"<<endl;
+ }
+ else{
+ cout<<"Pair is not found";
+ }
+    return 0;
+}
