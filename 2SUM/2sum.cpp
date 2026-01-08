3835da2df917940b8b540102a0cd48d997d2fa6e
Piyush_Dubey <pd3429978@gmail.com>
2026-01-08 16:59:17 +0530

    Add files via upload

---
+++ 2SUM/2sum.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ bool findpairwithsum(vector<int>&arr,int target){
+    int start=0;
+    int end=arr.size()-1;
+
+    while(start<end){
+        int sum = arr[start]+arr[end];
+        if(sum==target){
+            return true;
+        }
+        else if (sum<target)
+        start++;
+        
+        else{
+            end--;
+        }
+         
+    }
+    return false ;
+
+ } 
+
+
+int main (){
+
+  vector <int>  arr={1,2,3,4,6,18,25};
+  int target =10;
+  if(findpairwithsum(arr,target)){
+    cout<<"Pair found"<<endl;
+  }
+  else{
+    cout<<"Pair is not  found"<<endl;
+  }
+
+    return 0;
+}
