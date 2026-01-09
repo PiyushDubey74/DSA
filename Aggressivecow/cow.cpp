ce9c35ecd64aa0d0c7ed65c402fba8ca247c9c09
Piyush_Dubey <pd3429978@gmail.com>
2026-01-09 17:58:02 +0530

    Add files via upload

---
+++ Aggressivecow/cow.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+ 
+    std::vector<int> stalls = {1, 2, 8, 4, 9};
+    int k = 3; 
+    int n = stalls.size();
+
+    
+    std::sort(stalls.begin(), stalls.end());
+
+
+    int start = 1;
+    int end = stalls[n - 1] - stalls[0];
+    int ans = 0; 
+
+  
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+   
+        int cowCount = 1;         
+        int lastPos = stalls[0];  
+
+        for (int i = 1; i < n; i++) {
+            
+            if (stalls[i] - lastPos >= mid) {
+               
+                cowCount++;
+                lastPos = stalls[i];
+            }
+        }
+       
+        if (cowCount < k) {
+            
+            end = mid - 1;
+        } else {
+          
+            ans = mid;
+            start = mid + 1;
+        }
+    }
+
+   
+    std::cout << "The largest minimum distance is: " << ans << std::endl;
+
+    return 0;
+}
