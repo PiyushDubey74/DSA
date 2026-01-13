3b2e34ce654bb85ddd6f0323da16aa357c09c92d
Piyush_Dubey <pd3429978@gmail.com>
2026-01-13 17:00:01 +0530

    Fix formatting and add missing newline at end of file

--- Function/Passbypointer2.cpp
+++ Function/Passbypointer2.cpp
@@ -4,7 +4,7 @@ using namespace std;
  void dob(int *p)
  {
     for(int i=0;i<5;i++){
-        p[i]=2*p[i];
+        p[i]=2 *p[i];
     }
  }
 
@@ -16,4 +16,5 @@ int main (){
  cout<<arr[i];
 
     return 0;
+
 }
