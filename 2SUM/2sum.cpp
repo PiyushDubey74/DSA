a16781d8c2a09a1c52e12ef9cb29822a62122c7e
Piyush_Dubey <pd3429978@gmail.com>
2026-01-13 17:01:37 +0530

    Refactor 2sum.cpp for improved readability

--- 2SUM/2sum.cpp
+++ 2SUM/2sum.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
  bool findpairwithsum(vector<int>&arr,int target){
-    int start=0;
+    int start =0;
     int end=arr.size()-1;
 
     while(start<end){
@@ -35,4 +35,5 @@ int main (){
   }
 
     return 0;
+
 }
