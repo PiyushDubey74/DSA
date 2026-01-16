9352c839a254befb1a076d3b91602d155dc5babd
Piyush_Dubey <pd3429978@gmail.com>
2026-01-16 15:41:36 +0530

    Fix formatting issues in pattern12.cpp

--- patterns/pattern12.cpp
+++ patterns/pattern12.cpp
@@ -6,7 +6,7 @@ int main (){
      cout<<"Enter a number";
      cin>>n;
       
-     int space =2*(n-1);
+     int space = 2*(n-1);
      for(int i=1;i<=n;i++){
         // number
       for(int j=1;j<=i;j++){
@@ -27,4 +27,5 @@ int main (){
 
 
     return 0;
+
 }
