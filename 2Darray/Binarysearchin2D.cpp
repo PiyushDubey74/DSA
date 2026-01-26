0979a06835fde122b9985124ee1540ab3587a2d5
Piyush_Dubey <pd3429978@gmail.com>
2026-01-26 16:07:00 +0530

    Update Binarysearchin2D.cpp

--- 2Darray/Binarysearchin2D.cpp
+++ 2Darray/Binarysearchin2D.cpp
@@ -4,7 +4,7 @@ using namespace std;
  void print2D(int arr[][4],int row){
     for(int i=0;i<row;i++){
       for(int j= 0;j<4;j++){
-        cout<<arr[i][j]<<" \t";
+        cout<<arr[i] [j]<<" \t";
       }
       cout<< endl;
     }
@@ -48,5 +48,6 @@ int main(){
     
 
 }
+
 
 
