b358e095995ad84e26253fcd5f94c574eefe839d
Piyush_Dubey <pd3429978@gmail.com>
2026-02-03 10:12:58 +0530

    Update 01.cpp

--- Binary Search/01.cpp
+++ Binary Search/01.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int BinarySearch(int arr[],int n,int key)
 {
-        int start=0,end=n-1,mid;
+        int start= 0,end=n-1,mid;
 
         while(start>=end){
             mid=start+(end-start)/2;
@@ -47,4 +47,5 @@ int main (){
 
 
     
+
 }
