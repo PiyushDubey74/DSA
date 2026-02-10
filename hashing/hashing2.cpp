874559bd40f48808f67161688676331704f93c6e
Piyush_Dubey <pd3429978@gmail.com>
2026-02-10 23:10:18 +0530

    Fix formatting issues in hashing2.cpp

--- hashing/hashing2.cpp
+++ hashing/hashing2.cpp
@@ -8,7 +8,7 @@ int main (){
    
      
    
-     int hash[26] ={ 0};
+     int hash[26 ] ={ 0};
      for(int i=0;i<s.size();i++){
         hash[s[i] -'a']++;
      }
@@ -26,4 +26,5 @@ int main (){
     return 0;
 
 }
+
 
