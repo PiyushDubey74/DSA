884101d1442575c4505241c2e563034e4f3bf3fe
Piyush_Dubey <pd3429978@gmail.com>
2026-02-24 23:24:09 +0530

    Update bubblesort.cpp

--- Sorting/bubblesort.cpp
+++ Sorting/bubblesort.cpp
@@ -45,7 +45,7 @@ int main (){
    for(int i =0;i<n;i++){
     cin>>arr[i];
    }
-   for(int i=n-2;i>=0;i--) 
+   for(int i= n-2;i>=0;i--) 
    {
     bool swapped =0;
     for(int j=0;j<=i;j++){
@@ -70,3 +70,4 @@ int main (){
 
 
 }
+
