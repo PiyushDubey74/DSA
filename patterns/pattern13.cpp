7945cc194587c1bb3f2ebe0bb840831f1c0ba130
Piyush_Dubey <pd3429978@gmail.com>
2026-02-25 21:58:58 +0530

    Fix formatting and clean up pattern13.cpp

--- patterns/pattern13.cpp
+++ patterns/pattern13.cpp
@@ -10,7 +10,7 @@ int main (){
    for(int i=1;i<=n;i++){
       
      for(int j=1;j<=i;j++){
-        cout<<num<<" ";
+        cout<< num<<" ";
         num=num+1;
      }
     //  for(int j=1;j<2*n+1;j++){
@@ -21,4 +21,5 @@ int main (){
    }
 
     return 0;
+
 }
