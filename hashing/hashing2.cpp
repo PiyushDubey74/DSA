9de16705f39021ea878b398684eba50c31515279
Piyush_Dubey <pd3429978@gmail.com>
2026-02-26 22:36:34 +0530

    Update hashing2.cpp

--- hashing/hashing2.cpp
+++ hashing/hashing2.cpp
@@ -8,7 +8,7 @@ int main (){
    
      
    
-     int hash[26 ] ={ 0};
+     int hash[ 26 ] ={ 0};
      for(int i=0;i<s.size();i++){
         hash[s[i] -'a']++;
      }
@@ -26,5 +26,6 @@ int main (){
     return 0;
 
 }
+
 
 
