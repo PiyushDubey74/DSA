c77539b9dd9dfe2bfaa8edfbeae13d7b27bd76f4
Piyush_Dubey <pd3429978@gmail.com>
2026-02-28 21:45:25 +0530

    Update ExceptionHandling.cpp

--- OOPM/ExceptionHandling.cpp
+++ OOPM/ExceptionHandling.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
     customer(string name ,int balance,int account_number)
     {
-        this->name =name;
+        this->name = name;
         this->balance=balance;
         this->account_number=account_number;
     };
@@ -65,5 +65,6 @@ int main()
     return 0;
 
 }
+
 
 
