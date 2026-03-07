10d3a61e635cb64b39040e94e407893304862983
Piyush_Dubey <pd3429978@gmail.com>
2026-03-07 22:01:48 +0530

    Update numberofoccurrance.cpp

--- Binary Search/numberofoccurrance.cpp
+++ Binary Search/numberofoccurrance.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int BinarySearch(int arr[],int n,int key){
 int start=0,end=n-1,mid;
-int first=-1,last=-1;
+int first=-1,last =-1;
 while(start<=end){
    
      mid=start+(end-start)/2;
@@ -72,3 +72,4 @@ int main (){
 
 
 }
+
