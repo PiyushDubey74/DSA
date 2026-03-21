d1e6ef331d8437191495df1b5b6c612a0971ac17
Piyush_Dubey <pd3429978@gmail.com>
2026-03-21 21:19:08 +0530

    Update selectionsort1.cpp

--- Sorting/selectionsort1.cpp
+++ Sorting/selectionsort1.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void selection_sort(int arr[],int n){
+void selection_sort(int arr[ ],int n){
     for(int i=0;i<=n-2;i++){
         int mini =i;
         for(int j=i;j<=n-1;j++){
