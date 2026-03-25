2ff915723cd3fcafc70578163b99768def2eee7f
Piyush_Dubey <pd3429978@gmail.com>
2026-03-25 21:23:00 +0530

    Update RotatematrixbyK.cpp

--- 2Darray/RotatematrixbyK.cpp
+++ 2Darray/RotatematrixbyK.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 
-void printmatrix(int arr[][4], int n){
+void printmatrix(int arr[ ][4], int n){
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             cout << arr[i][j] << "\t";
