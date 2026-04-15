5e6d1fb526a4fc939037f74c1fa67b5d3e8e9ae9
Piyush_Dubey <pd3429978@gmail.com>
2026-04-15 22:30:12 +0530

    Update sortarr.cpp

--- sortarr.cpp
+++ sortarr.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool isSorted(int arr[], int size) {
-    for (int i = 1; i < size; ++i) {
+    for (int i = 1; i <size; ++i) {
         if (arr[i] < arr[i - 1]) {
             return false;
         }
